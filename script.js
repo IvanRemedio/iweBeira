1b2538c662293f76d50a4e1f473da7c9c81dc61a
IvanRemedio <114109191+IvanRemedio@users.noreply.github.com>
2026-01-28 12:07:20 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,3 @@
-let map;
-let marker;
-let accuracyCircle;
-
-function capturarLocalizacao() {
-  navigator.geolocation.getCurrentPosition(
-    (pos) => {
-      alert(
-        "Lat: " + pos.coords.latitude +
-        "\nLng: " + pos.coords.longitude +
-        "\nPrecisão: ±" + Math.round(pos.coords.accuracy) + "m"
-      );
-    },
-    () => alert("Erro"),
-    { enableHighAccuracy: true }
-  );
+function teste() {
+  alert("Funcionou!");
 }
