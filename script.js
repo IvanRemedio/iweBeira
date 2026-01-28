03b88e8c0ddea48834e004bb88513527cc5f9d81
IvanRemedio <114109191+IvanRemedio@users.noreply.github.com>
2026-01-28 12:03:14 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,32 +1,17 @@
-let accuracyCircle;
 let map;
 let marker;
+let accuracyCircle;
 
-function mostrarMapa(lat, lng, accuracy) {
-  if (!map) {
-    map = L.map("map").setView([lat, lng], 18);
-
-    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-      attribution: "© OpenStreetMap"
-    }).addTo(map);
-  }
-
-  if (marker) {
-    marker.setLatLng([lat, lng]);
-  } else {
-    marker = L.marker([lat, lng])
-      .addTo(map)
-      .bindPopup(`📍 Local aproximado do lixo<br>Precisão: ±${Math.round(accuracy)}m`)
-      .openPopup();
-  }
-
-  if (accuracyCircle) {
-    accuracyCircle.setLatLng([lat, lng]).setRadius(accuracy);
-  } else {
-    accuracyCircle = L.circle([lat, lng], {
-      radius: accuracy,
-      color: "blue",
-      fillOpacity: 0.2
-    }).addTo(map);
-  }
+function capturarLocalizacao() {
+  navigator.geolocation.getCurrentPosition(
+    (pos) => {
+      alert(
+        "Lat: " + pos.coords.latitude +
+        "\nLng: " + pos.coords.longitude +
+        "\nPrecisão: ±" + Math.round(pos.coords.accuracy) + "m"
+      );
+    },
+    () => alert("Erro"),
+    { enableHighAccuracy: true }
+  );
 }
