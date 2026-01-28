86699082dc10f4f2a48c7bfd5fa68e0fdf7ee123
IvanRemedio <114109191+IvanRemedio@users.noreply.github.com>
2026-01-28 12:52:34 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,56 +1,17 @@
-let map;
-let marker;
+function enviarParaForm() {
+  const lat = localStorage.getItem("lat");
+  const lng = localStorage.getItem("lng");
+  const acc = localStorage.getItem("acc");
 
-// Inicializa o mapa (posição genérica inicial)
-map = L.map('map').setView([0, 0], 2);
-
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  attribution: '© OpenStreetMap'
-}).addTo(map);
-
-// Botão
-document
-  .getElementById("btnLocalizacao")
-  .addEventListener("click", capturarLocalizacao);
-
-function capturarLocalizacao() {
-
-  if (!navigator.geolocation) {
-    alert("Geolocalização não suportada pelo navegador");
+  if (!lat) {
+    alert("Capture a localização primeiro");
     return;
   }
 
-  navigator.geolocation.getCurrentPosition(
-    sucesso,
-    erro,
-    {
-      enableHighAccuracy: true,
-      timeout: 15000,
-      maximumAge: 0
-    }
-  );
-}
+  const formURL = `https://docs.google.com/forms/d/e/1KN7ZEuKXsB1oZds6/formResponse
+  ?entry.111111=${lat}
+  &entry.222222=${lng}
+  &entry.333333=${Math.round(acc)}`;
 
-function sucesso(pos) {
-  const lat = pos.coords.latitude;
-  const lng = pos.coords.longitude;
-  const acc = pos.coords.accuracy;
-
-  document.getElementById("info").innerHTML =
-    `Latitude: ${lat}<br>
-     Longitude: ${lng}<br>
-     Precisão aproximada: ${Math.round(acc)} metros`;
-
-  map.setView([lat, lng], 18);
-
-  if (!marker) {
-    marker = L.marker([lat, lng]).addTo(map);
-  } else {
-    marker.setLatLng([lat, lng]);
-  }
-}
-
-function erro(err) {
-  console.error(err);
-  alert("Não foi possível obter a localização");
+  window.open(formURL, "_blank");
 }
